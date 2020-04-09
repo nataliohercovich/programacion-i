16e6110bdee222e845de35b2f7fa02ddb5fde980
valenscalco <mv.scalco@hotmail.com>
2020-04-09 14:48:19 -0300

    commit

--- 58103-Scalco-Valentina/Ej5.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Ingrese un numero\n"))
-for i in range(1, num+1, 2):
-    for j in range(i, 0, -2):
-        print(j, end=" ")
-    print(" ")
--- 58103-Scalco-Valentina/Ej8.py
+++
@@ -1,6 +0,0 @@
-lista = ['Matematica','Lengua','Física','Química','Historia']
-notas = []
-for materia in lista:
-    notas.append(input("¿Que nota se tiene en " + materia + "?\n"))
-for i in range(len(lista)):
-    print("En", lista[i], "tiene un", notas[i])
