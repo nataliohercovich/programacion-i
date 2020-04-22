fa626649c3697bbcbbaa6dfabfed4207fde8bc53
porollansantiago <santiporollan@gmail.com>
2020-04-22 15:13:30 -0300

    agregado Cajero_Automatico

---
+++ 57031-porollan-santiago/tp2/Cajero_automatico.py
@@ -0,0 +1,15 @@
+
+
+class Cajero_automatico():
+    def __init__(self):
+        pass
+
+    def agregar_dinero(billetes):
+        for billete in billetes:
+            pass
+
+    def contar_dinero():
+        pass
+
+    def extraer_dinero(monto):
+        pass
