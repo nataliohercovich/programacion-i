c0e1b0f1f0c9668652fb6905d5cfc2a4a76dd6ae
AGauchat <agus123456@hotmail.com>
2020-04-29 18:51:15 -0300

    TP2

--- 58052-agustin,gauchat/Tp2/Ej1/Cajero.py
+++ 58052-agustin,gauchat/Tp2/Ej1/Cajero.py
@@ -2,7 +2,10 @@ from Billetes import *
 
 class Cajero():
     def __init__(self):
+        self.verificacion = 0
+        self.a = True
         self.fondos = 0
+        self.monto = 0
         self.exctraccion = []
         self.cantBilletes = {
             #[cantBill, valorTotal]
@@ -36,113 +39,140 @@ class Cajero():
 
     def contarDinero(self):
         #100 200 500 1000
-        cont = [0, 0, 0, 0]
+        cont = [[0, 0], [0, 0], [0, 0], [0, 0]]
         if self.cantBilletes["$100"][0] != 0:
-            cont[0] = self.cantBilletes["$100"][0]
+            cont[0][0] = self.cantBilletes["$100"][0]
+            cont[0][1] = self.cantBilletes["$100"][1]
         if self.cantBilletes["$200"][0] != 0:
-            cont[1] = self.cantBilletes["$200"][0]
+            cont[1][0] = self.cantBilletes["$200"][0]
+            cont[1][1] = self.cantBilletes["$200"][1]
         if self.cantBilletes["$500"][0] != 0:
-            cont[2] = self.cantBilletes["$500"][0]
+            cont[2][0] = self.cantBilletes["$500"][0]
+            cont[2][1] = self.cantBilletes["$500"][1]
         if self.cantBilletes["$1000"][0] != 0:
-            cont[3] = self.cantBilletes["$1000"][0]
+            cont[3][0] = self.cantBilletes["$1000"][0]
+            cont[3][1] = self.cantBilletes["$1000"][1]
 
-        a = "Hay " + str(cont[0]) + " billetes de 100\n" + "Hay " + str(cont[1]) + " billetes de 200\n" +  "Hay " + str(cont[2]) + " billetes de 500\n" + "Hay " + str(cont[3]) + " billetes de 1000\n" + "Total: "+ str(self.fondos)
+        a = "Hay " + str(cont[0][0]) + " billetes de 100, total: " + str(cont[0][1]) + "\n" + "Hay " + str(cont[1][0]) + " billetes de 200, total: " + str(cont[1][1]) + "\n" + "Hay " + str(cont[2][0]) + " billetes de 500, total: " + str(cont[2][1]) + "\n" + "Hay " + str(cont[3][0]) + " billetes de 1000, total: " + str(cont[3][1]) + "\n" + "Total: "+ str(self.fondos)
 
         return str(a)
-
-        
-
-
-    def extraerDinero(self, monto, cambio, porcentaje):
-        if monto > self.fondos:
-            return("No hay suficientes fondos")
-        if monto%100 != 0:
-            return("El monto no es multiplo de 100")
-        
-        salida = True
-        cantidad = monto
-
-        while salida:
-            if cambio == True:
-                self.calcularCambio(porcentaje, monto)
-
-            if monto >= 1000 and self.cantBilletes["$1000"][0] != 0:
-                self.cantBilletes["$1000"][0] -= 1
-                self.cantBilletes["$1000"][1] -= 1000
-                self.fondos -= 1000
-                monto -= 1000
-                self.exctraccion.append("$1000")
-
-            if monto >= 500 and self.cantBilletes["$500"][0] != 0:
-                self.cantBilletes["$500"][0] -= 1
-                self.cantBilletes["$500"][1] -= 500
-                self.fondos -= 500
-                monto -= 500
-                self.exctraccion.append("$500")
-
-            if monto >= 200 and self.cantBilletes["$200"][0] != 0:
-                self.cantBilletes["$200"][0] -= 1
-                self.cantBilletes["$200"][1] -= 200
-                self.fondos -= 200
-                monto -= 200
-                self.exctraccion.append("$200")
-
-            if monto >= 100 and self.cantBilletes["$100"][0] != 0:
+    
+    def cambio(self, cambio):
+        if cambio >= 100 and self.cantBilletes["$100"][0] != 0:
+            while self.cantBilletes["$100"][0] != 0 and cambio >= 100:
                 self.cantBilletes["$100"][0] -= 1
                 self.cantBilletes["$100"][1] -= 100
                 self.fondos -= 100
-                monto -= 100
+                cambio -= 100
+                self.monto -= 100
                 self.exctraccion.append("$100")
 
-            if monto == 0:
-                salida = False
+        if cambio >= 200 and self.cantBilletes["$200"][0] != 0:
+            while self.cantBilletes["$200"][0] != 0 and cambio >= 200:
+                self.cantBilletes["$200"][0] -= 1
+                self.cantBilletes["$200"][1] -= 200
+                self.fondos -= 200
+                cambio -= 200
+                self.monto -= 200
+                self.exctraccion.append("$200")
 
-            return(self.exctraccion)
+        if cambio >= 500 and self.cantBilletes["$500"][0] != 0:
+            while self.cantBilletes["$500"][0] != 0 and cambio >= 500:
+                self.cantBilletes["$500"][0] -= 1
+                self.cantBilletes["$500"][1] -= 500
+                self.fondos -= 500
+                cambio -= 500
+                self.monto -= 500
+                self.exctraccion.append("$500")
 
-        def cambio(self, cambio, monto):
-            if cambio >= 100 and self.cantBilletes["$100"][0] != 0:
-                while self.cantBilletes["$100"][0] != 0 and cambio != 0:
-                    self.cantBilletes["$100"][0] -= 1
-                    self.cantBilletes["$100"][1] -= 100
-                    self.fondos -= 100
-                    cambio -= 100
-                    monto -= 100
-                    self.exctraccion.append("$100")
+        if cambio >= 1000 and self.cantBilletes["$1000"][0] != 0:
+            while self.cantBilletes["$1000"][0] != 0 and cambio >= 1000:
+                self.cantBilletes["$1000"][0] -= 1
+                self.cantBilletes["$1000"][1] -= 100
+                self.fondos -= 1000
+                cambio -= 1000
+                self.monto -= 1000
+                self.exctraccion.append("$1000")
 
-            if cambio >= 200 and self.cantBilletes["$200"][0] != 0:
-                while self.cantBilletes["$200"][0] != 0 and cambio != 0:
-                    self.cantBilletes["$200"][0] -= 1
-                    self.cantBilletes["$200"][1] -= 200
-                    self.fondos -= 200
-                    cambio -= 200
-                    monto -= 200
-                    self.exctraccion.append("$200")
+        if cambio != 0:
+            cambio += 100
+            self.cambio(cambio)
+            return self.exctraccion
 
-            if cambio >= 500 and self.cantBilletes["$500"][0] != 0:
-                while self.cantBilletes["$500"][0] != 0 and cambio != 0:
-                    self.cantBilletes["$500"][0] -= 1
-                    self.cantBilletes["$500"][1] -= 500
-                    self.fondos -= 500
-                    cambio -= 500
-                    monto -= 500
-                    self.exctraccion.append("$500")
+        self.extraerDinero(self.monto, False, 0)
 
-            if cambio >= 1000 and self.cantBilletes["$1000"][0] != 0:
-                while self.cantBilletes["$1000"][0] != 0 and cambio != 0:
-                    self.cantBilletes["$1000"][0] -= 1
-                    self.cantBilletes["$1000"][1] -= 100
-                    self.fondos -= 1000
-                    cambio -= 1000
-                    monto -= 1000
-                    self.exctraccion.append("$1000")
+    def calcularCambio(self, porcentaje):
+        cambio = self.monto * (porcentaje / 100)
+        if cambio < 100:
+            cambio = 100
+        else:
+            cambio = int(100 * round(float(cambio)/100))
+        self.cambio(cambio)
+        return
 
-            if cambio != 0:
-                cambio += 100
-                self.cambio(cambio, monto)
-                
-            self.extraerDinero(monto, False, 0)
+    def extraerDinero(self, monto, cambio, porcentaje):
+        self.monto = monto
+        if self.a == True:
+            self.verificacion = self.monto
+            self.a = False
 
-        def calcularCambio(self, porcentaje, monto):
-            cambio = monto * (1 - (porcentaje / 100))
-            cambio = int(100 * round(float(cambio)/base))
-            self.cambio(cambio, monto)
+        if self.monto > self.fondos:
+            return("No hay suficientes fondos")
+        if self.monto%100 != 0:
+            return("El monto no es multiplo de 100")
+        if porcentaje > 100:
+            return("El procentaje debe ir de 0 a 100")
+        
+
+        
+        if cambio == True and porcentaje < 100:
+            self.calcularCambio(porcentaje)
+            return self.exctraccion
+
+        if self.monto >= 1000 and self.cantBilletes["$1000"][0] != 0:
+            while self.cantBilletes["$1000"][0] != 0 and self.monto >= 1000:
+                self.cantBilletes["$1000"][0] -= 1
+                self.cantBilletes["$1000"][1] -= 100
+                self.fondos -= 1000
+                self.monto -= 1000
+                self.exctraccion.append("$1000")
+
+        if self.monto >= 500 and self.cantBilletes["$500"][0] != 0:
+            while self.cantBilletes["$500"][0] != 0 and self.monto >= 500:
+                self.cantBilletes["$500"][0] -= 1
+                self.cantBilletes["$500"][1] -= 500
+                self.fondos -= 500
+                self.monto -= 500
+                self.exctraccion.append("$500")
+
+        if self.monto >= 200 and self.cantBilletes["$200"][0] != 0:
+            while self.cantBilletes["$200"][0] != 0 and self.monto >= 200:
+                self.cantBilletes["$200"][0] -= 1
+                self.cantBilletes["$200"][1] -= 200
+                self.fondos -= 200
+                self.monto -= 200
+                self.exctraccion.append("$200")
+
+        if self.monto >= 100 and self.cantBilletes["$100"][0] != 0:
+            while self.cantBilletes["$100"][0] != 0 and self.monto >= 100:
+                self.cantBilletes["$100"][0] -= 1
+                self.cantBilletes["$100"][1] -= 100
+                self.fondos -= 100
+                self.monto -= 100
+                self.exctraccion.append("$100")
+
+        if self.monto != 0:
+            self.monto += 100
+            self.extraerDinero(self.monto, False, 0)
+        
+        ver = ' '.join([str(elem) for elem in self.exctraccion])
+        ver = ver.replace("$", "")
+        ver = ver.split(" ")
+        verif = 0
+        for i in ver:
+            verif += int(i)
+
+        if verif == self.verificacion:
+            return self.exctraccion
+        else:
+            return "No hay billetes para esa extraccion"
--- 58052-agustin,gauchat/Tp2/Ej1/Index.py
+++ 58052-agustin,gauchat/Tp2/Ej1/Index.py
@@ -4,12 +4,10 @@ if __name__ == "__main__":
     cajero = Cajero()
 
     cajero.agregarDinero([
-        #Total: 7400
-        billete1000(), billete1000(),
-        billete500(), billete500(), billete500(),
-        billete200(), billete200(),
-        billete100(), billete100(), billete100(), billete100(), billete100()
+        #Total: 4400
+        billete500(), billete500(), billete500()
         ])
 
-    print(cajero.extraerDinero(1200))
+    print(cajero.contarDinero() + "\n")
+    print(cajero.extraerDinero(1200, False, 0))
     print(cajero.contarDinero())
